db9908455bb6e5d679a35fbddcb622ac6769520b
Hoang Son <hoangvansonit@gmail.com>
2022-07-20 15:13:39 +0700

    render list staff

--- src/components/StaffList.js
+++ src/components/StaffList.js
@@ -1,8 +1,32 @@
 import React from 'react';
-
+import { Card, CardImg, CardTitle, CardBody } from 'reactstrap';
 class StaffList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
   render() {
-    return <div></div>;
+    const staffList = this.props.staffs.map(staff => {
+      return (
+        <div key={staff.id}>
+          <Card>
+            <CardBody>
+              <CardImg src={staff.image} alt={staff.name} />
+              <CardTitle className='text-center mt-4'>{staff.name}</CardTitle>
+            </CardBody>
+          </Card>
+        </div>
+      );
+    });
+    return (
+      <div className='container'>
+        <div className='row mt-3'>
+          <h5>Bấm vào ảnh hoặc tên nhân viên để xem thông tin</h5>
+        </div>
+        <div className='row'>{staffList}</div>
+      </div>
+    );
   }
 }
 
