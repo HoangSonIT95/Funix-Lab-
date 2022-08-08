911e8b35a6db873b800c401bf0d949bdab87f1d7
Hoang Son <hoangvansonit@gmail.com>
2022-08-08 13:22:26 +0700

    react thunk & react redux forms revisited

--- src/components/ContactComponent.js
+++ src/components/ContactComponent.js
@@ -3,16 +3,12 @@ import {
   Breadcrumb,
   BreadcrumbItem,
   Button,
-  Form,
-  FormGroup,
   Label,
-  Input,
   Col,
   Row,
-  FormFeedback,
 } from 'reactstrap';
 import { Link } from 'react-router-dom';
-import { Control, LocalForm, Errors } from 'react-redux-form';
+import { Control, Form, Errors } from 'react-redux-form';
 
 const required = val => val && val.length;
 const maxLength = len => val => !val || val.length <= len;
@@ -28,7 +24,7 @@ class Contact extends React.Component {
   handleSubmit(values) {
     console.log('Current State is: ' + JSON.stringify(values));
     alert('Current State is: ' + JSON.stringify(values));
-    // event.preventDefault();
+    this.props.resetFeedbackForm();
   }
 
   render() {
@@ -97,7 +93,10 @@ class Contact extends React.Component {
             <h3>Send us your Feedback</h3>
           </div>
           <div className='col-12 col-md-9'>
-            <LocalForm onSubmit={values => this.handleSubmit(values)}>
+            <Form
+              model='feedback'
+              onSubmit={values => this.handleSubmit(values)}
+            >
               <Row className='form-group'>
                 <Label htmlFor='firstname' md={2}>
                   First Name
@@ -235,7 +234,7 @@ class Contact extends React.Component {
                   </Button>
                 </Col>
               </Row>
-            </LocalForm>
+            </Form>
           </div>
         </div>
       </div>
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -8,10 +8,8 @@ import Footer from './FooterComponent';
 import Contact from './ContactComponent';
 import About from './AboutComponent';
 import DishDetail from './DishdetailComponent';
-import { DISHES } from '../shared/dishes';
-import { COMMENTS } from '../shared/comments';
-import { PROMOTIONS } from '../shared/promotions';
-import { LEADERS } from '../shared/leaders';
+import { addComment, fetchDishes } from '../redux/ActionCreators';
+import { actions } from 'react-redux-form';
 
 const mapStateToProps = state => {
   return {
@@ -22,12 +20,32 @@ const mapStateToProps = state => {
   };
 };
 
+const mapDispatchToProps = dispatch => ({
+  addComment: (dishId, rating, author, comment) =>
+    dispatch(addComment(dishId, rating, author, comment)),
+  fetchDishes: () => {
+    dispatch(fetchDishes());
+  },
+  resetFeedbackForm: () => {
+    dispatch(actions.reset('feedback'));
+  },
+});
 class Main extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  componentDidMount() {
+    this.props.fetchDishes();
+  }
+
   render() {
-    const HomePage = props => {
+    const HomePage = () => {
       return (
         <Home
-          dish={this.props.dishes.filter(dish => dish.featured)[0]}
+          dish={this.props.dishes.dishes.filter(dish => dish.featured)[0]}
+          dishesLoading={this.props.dishes.isLoading}
+          dishesErrMess={this.props.dishes.errMess}
           promotion={this.props.promotions.filter(promo => promo.featured)[0]}
           leader={this.props.leaders.filter(leader => leader.featured)[0]}
         />
@@ -38,13 +56,16 @@ class Main extends Component {
       return (
         <DishDetail
           dish={
-            this.props.dishes.filter(
+            this.props.dishes.dishes.filter(
               dish => dish.id === parseInt(match.params.dishId, 10)
             )[0]
           }
+          isLoading={this.props.dishes.isLoading}
+          errMess={this.props.dishes.errMess}
           comments={this.props.comments.filter(
             comment => comment.dishId === parseInt(match.params.dishId, 10)
           )}
+          addComment={this.props.addComment}
         />
       );
     };
@@ -60,7 +81,13 @@ class Main extends Component {
             path='/menu'
             component={() => <Menu dishes={this.props.dishes} />}
           />
-          <Route exact path='/contactus' component={Contact} />
+          <Route
+            exact
+            path='/contactus'
+            component={() => (
+              <Contact resetFeedbackForm={this.props.resetFeedbackForm} />
+            )}
+          />
           <Route
             exact
             path='/aboutus'
@@ -74,4 +101,4 @@ class Main extends Component {
   }
 }
 
-export default withRouter(connect(mapStateToProps)(Main));
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(Main));
