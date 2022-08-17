3461cd8c86c41fe5b921f9e05012d532ff66ea2f
Hoang Son <hoangvansonit@gmail.com>
2022-08-17 14:44:49 +0700

    Single Page Applications Part 1

--- src/App.js
+++ src/App.js
@@ -1,19 +1,9 @@
 import React, { Component } from 'react';
-import { Navbar, NavbarBrand } from 'reactstrap';
-import Menu from './components/MenuComponent';
-import { DISHES } from './shared/dishes';
 import Main from './components/MainComponent';
 import './App.css';
 
 import { BrowserRouter } from 'react-router-dom';
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      dishes: DISHES,
-    };
-  }
-
   render() {
     return (
       <BrowserRouter>
